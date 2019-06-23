f5715201cac01042fc14d1704e1697d49f0b92ec
abdosami1500 <44530940+abdosami1500@users.noreply.github.com>
2019-06-23 20:06:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const myid = [''];
-const prefix = ['-'];
+const myid = [551422413108281345];
+const prefix = [!];
 
 client.on('ready', () => {
    console.log(`----------------`);
